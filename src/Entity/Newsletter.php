daadcabe6eb02a7f31553c0328248c90d74d029c
Paul Nganda <paulngandasmith@gmail.com>
2024-09-15 18:07:29 +0100

    Feat: newsletter post

--- src/Entity/Newsletter.php
+++ src/Entity/Newsletter.php
@@ -2,11 +2,22 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
 use App\Repository\NewsletterRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: NewsletterRepository::class)]
+#[UniqueEntity(fields: ['email'], message: "Cet email {{ value }} est déjà enregistré")]
+#[ApiResource(
+    operations: [
+        new Post(
+            denormalizationContext: ['groups' => ['newsletter:create']],
+        ),
+    ]
+)]
 class Newsletter
 {
     #[ORM\Id]
@@ -14,11 +25,9 @@ class Newsletter
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $title = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $content = null;
+    #[ORM\Column(length: 180)]
+    #[Groups(['newsletter:create'])]
+    private ?string $email = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
@@ -28,26 +37,14 @@ class Newsletter
         return $this->id;
     }
 
-    public function getTitle(): ?string
+    public function getEmail(): ?string
     {
-        return $this->title;
+        return $this->email;
     }
 
-    public function setTitle(string $title): static
+    public function setEmail(string $email): static
     {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
-
-    public function setContent(string $content): static
-    {
-        $this->content = $content;
+        $this->email = $email;
 
         return $this;
     }
