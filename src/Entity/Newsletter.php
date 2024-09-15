ce726e5d2604e14dc92a52ad85b8c7f49a658446
Paul Nganda <paulngandasmith@gmail.com>
2024-09-15 18:17:14 +0100

    newsletter

--- src/Entity/Newsletter.php
+++ src/Entity/Newsletter.php
@@ -32,6 +32,11 @@ class Newsletter
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
